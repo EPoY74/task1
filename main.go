e2222eb542fe0c7b0a94df7ba9350c4767035861
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-25 09:47:10 +0500

    Добавляю WaitGroup, что бы завершать goрутины и был предсказуема яих работа

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 // Трогаем goрутины  по уроку
 // https://www.youtube.com/watch?v=Qf24zMzMmgI
 func main() {
+	wg := sync.WaitGroup{}
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go fmt.Println(i)
 	}
 
